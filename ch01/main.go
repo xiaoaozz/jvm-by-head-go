b487fad513b0cd57e9853f9495a1e21abd91be8b
zal <3189137314@qq.com>
2023-09-24 23:46:12 +0800

    test：测试github提交代码

--- ch01/main.go
+++ ch01/main.go
@@ -7,6 +7,8 @@ func main() {
 	if cmd.versionFlag {
 		fmt.Printf("version 0.0.0.1")
 	} else if cmd.helpFlag || cmd.class == "" {
+		// 测试github提交代码
+		fmt.Printf("github提交代码 ")
 		printUsage()
 	} else {
 		startJVM(cmd)

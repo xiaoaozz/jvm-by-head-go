5e0ffa29573bf3d840cdb95bbab5e3c88fceabc4
xiaoaozz <3189137314@qq.com>
2023-10-21 18:51:07 +0800

    update：常量池定义增加

---
+++ ch03/classfile/class_file.go
@@ -0,0 +1,118 @@
+package classfile
+
+import "fmt"
+
+// ClassFile Java虚拟机定义的class文件规范
+type ClassFile struct {
+	minorVersion uint16
+	majorVersion uint16
+	constantPool ConstantPool
+	accessFlags  uint16
+	thisClass    uint16
+	superClass   uint16
+	interfaces   []uint16
+	fields       []*MemberInfo
+	methods      []*MemberInfo
+	attributes   []AttributeInfo
+}
+
+// Parse 将[]byte解析成ClassFile结构体
+func Parse(classData []byte) (cf *ClassFile, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			var ok bool
+			err, ok = r.(error)
+			if !ok {
+				err = fmt.Errorf("%v", r)
+			}
+		}
+	}()
+	cr := &ClassReader{classData}
+	cf = &ClassFile{}
+	cf.read(cr)
+	return
+}
+
+// read 依次调用其他方法解析class文件
+func (self *ClassFile) read(reader *ClassReader) {
+	self.readAndCheckMagic(reader)
+	self.readAndCheckVersion(reader)
+	self.constantPool = readConstantPool(reader)
+	self.accessFlags = reader.readUint16()
+	self.thisClass = reader.readUint16()
+	self.superClass = reader.readUint16()
+	self.interClass = reader.readUint16s()
+	self.fields = readMembers(reader, self.constantPool)
+	self.methods = readMembers(reader, self.constantPool)
+	self.attributes = readAttributes(reader, self.constantPool)
+}
+
+// readAndCheckMagic 读取并且检查魔数
+func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
+	magic := reader.readUint32()
+	// 如果class文件开头的魔数不是0xCAFEBABE，则暂时先终止程序执行
+	if magic != 0xCAFEBABE {
+		panic("java.lang.ClassFormatError: magic!")
+	}
+}
+
+// readAndCheckVersion 读取并且检查版本号
+func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
+	self.minorVersion = reader.readUint16()
+	self.majorVersion = reader.readUint16()
+	// java se 支持版本号为 45-52 的class文件
+	// 而副版本号一般都是0
+	switch self.majorVersion {
+	case 45:
+		return
+	case 46, 47, 48, 49, 50, 51, 52:
+		if self.minorVersion == 0 {
+			return
+		}
+	}
+	panic("java.lang.UnsupportedClassVersionError!")
+}
+
+func (self *ClassFile) MinorVersion() uint16 {
+	return self.minorVersion
+}
+func (self *ClassFile) MajorVersion() uint16 {
+	return self.majorVersion
+}
+func (self *ClassFile) ConstantPool() ConstantPool {
+	return self.constantPool
+}
+
+func (self *ClassFile) AccessFlags() uint16 {
+	return self.accessFlags
+}
+
+func (self *ClassFile) Fields() []*MemberInfo {
+	return self.fields
+}
+func (self *ClassFile) Methods() []*MemberInfo {
+	return self.methods
+}
+
+// ClassName 从常量池查找类名
+func (self *ClassFile) ClassName() string {
+	return self.constantPool.getClassName(self.thisClass)
+}
+
+//
+func (self *ClassFile) superClassName() string {
+	if self.superClass > 0 {
+		return self.constantPool.getClassName(self.superClass)
+	}
+	// 只有java.lang.Object没有超类
+	return ""
+}
+
+// InterfaceNames 从常量池查找接口名
+func (self *ClassFile) InterfaceNames() []string {
+	interfaceNames := make([]string, len(self.interClass))
+	for i, cpIndex := range self.interfaces {
+		interfaceNames[i] = self.constantPool.getClassName(cpIndex)
+	}
+	return interfaceNames
+}

dfb753e762a28dac36c42c7c4c857e606b4af5c5
zal <3189137314@qq.com>
2023-10-21 15:53:36 +0800

    update: 完善classReader

--- ch03/classfile/class_reader.go
+++ ch03/classfile/class_reader.go
@@ -8,6 +8,7 @@ type ClassReader struct {
 
 // readUint8 读取u1类型数据
 func (self *ClassReader) readUint8() uint8 {
+	// 从字节数组中读取一个字节（8位），返回改字节的无符号整数值
 	val := self.data[0]
 	self.data = self.data[1:]
 	return val
@@ -15,6 +16,7 @@ func (self *ClassReader) readUint8() uint8 {
 
 // readUint16 读取u2类型数据
 func (self *ClassReader) readUint16() uint16 {
+	// 从字节数组中读取两个字节（16位），以大端字节序解析为无符号整数值，然后返回该值
 	val := binary.BigEndian.Uint16(self.data)
 	self.data = self.data[2:]
 	return val
@@ -34,6 +36,19 @@ func (self *ClassReader) readUint64() uint64 {
 	return val
 }
 
+// readUint16s 读取uint16的集合数据
 func (self *ClassReader) readUint16s() []uint16 {
-	return nil
+	n := self.readUint16()
+	s := make([]uint16, n)
+	for i := range s {
+		s[i] = self.readUint16()
+	}
+	return s
+}
+
+// readBytes 读取指定数量的字节数据
+func (self *ClassReader) readBytes(n uint32) []byte {
+	bytes := self.data[:n]
+	self.data = self.data[n:]
+	return bytes
 }

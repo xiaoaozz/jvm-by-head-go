5cff484c1275d60a757978defd974b08ddb00296
fuziran716 <1413552973@qq.com>
2023-12-19 15:21:27 +0800

    update:转换指令构建

---
+++ ch05/instructions/conversions/d2x.go
@@ -0,0 +1,35 @@
+package conversions
+
+import (
+	"jvm-by-head-go/ch04/rtda"
+	"jvm-by-head-go/ch05/instructions/base"
+)
+
+type D2F struct{ base.NoOperandsInstruction }
+
+func (self *D2F) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	d := stack.PopDouble()
+	f := float32(d)
+	stack.PushFloat(f)
+}
+
+// D2I double to int
+type D2I struct{ base.NoOperandsInstruction }
+
+func (self *D2I) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	d := stack.PopDouble()
+	i := int32(d)
+	stack.PushInt(i)
+}
+
+// D2L double to long
+type D2L struct{ base.NoOperandsInstruction }
+
+func (self *D2L) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	d := stack.PopDouble()
+	l := int64(d)
+	stack.PushLong(l)
+}
---
+++ ch05/instructions/conversions/l2x.go
@@ -0,0 +1,36 @@
+package conversions
+
+import (
+	"jvm-by-head-go/ch04/rtda"
+	"jvm-by-head-go/ch05/instructions/base"
+)
+
+// L2D long to double
+type L2D struct{ base.NoOperandsInstruction }
+
+func (self *L2D) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	l := stack.PopLong()
+	d := float64(l)
+	stack.PushDouble(d)
+}
+
+// L2F long to float
+type L2F struct{ base.NoOperandsInstruction }
+
+func (self *L2F) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	l := stack.PopLong()
+	f := float32(l)
+	stack.PushFloat(f)
+}
+
+// L2I long to int
+type L2I struct{ base.NoOperandsInstruction }
+
+func (self *L2I) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	l := stack.PopLong()
+	i := int32(l)
+	stack.PushInt(i)
+}

777ace0adf4653a66e9d74314aab96969b8246e0
fuziran716 <1413552973@qq.com>
2023-12-19 16:19:34 +0800

    update:扩展指令构建

---
+++ ch05/instructions/extended/ifnull.go
@@ -0,0 +1,28 @@
+package extended
+
+import (
+	"jvm-by-head-go/ch05/instructions/base"
+	"jvm-by-head-go/ch05/rtda"
+)
+
+type IFNULL struct {
+	base.BranchInstruction
+}
+
+func (self *IFNULL) Execute(frame *rtda.Frame) {
+	ref := frame.OperandStack().PopRef()
+	if ref == nil {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+type IFNONNULL struct {
+	base.BranchInstruction
+}
+
+func (self *IFNONNULL) Execute(frame *rtda.Frame) {
+	ref := frame.OperandStack().PopRef()
+	if ref != nil {
+		base.Branch(frame, self.Offset)
+	}
+}
